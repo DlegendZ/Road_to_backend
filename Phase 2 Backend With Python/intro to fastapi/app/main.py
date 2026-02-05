cb157423a7ead6e157537ee2b81b8bcbb11e03ef
DlegendZ <raynaldarvanlim@gmail.com>
2026-02-05 12:29:33 +0700

    http methods

--- Phase 2 Backend With Python/intro to fastapi/app/main.py
+++ Phase 2 Backend With Python/intro to fastapi/app/main.py
@@ -5,3 +5,30 @@ app = FastAPI()
 @app.get("/")
 def root() :
     return {"Status": "FastAPI is running"}
+
+@app.get("/users")
+def get_users():
+    return {"displayed users"}
+
+@app.get("/users/{user_id}")
+def get_user_by_id(user_id: int):
+    return { "user_id": user_id}
+
+@app.get("/users-query")
+def get_user_query(active: bool, age: int):
+    return { "active": active, "age": age}
+
+@app.post("/users")
+def create_user(user:dict) :
+    return {"created": user}
+
+@app.put("/users/{user_id}")
+def update_user(user_id: int, user: dict):
+    return {
+        "id": user_id,
+        "updated": user
+    }
+
+@app.delete("/users/{user_id}")
+def delete_user(user_id: int) :
+    return {"deleted_id": user_id}
